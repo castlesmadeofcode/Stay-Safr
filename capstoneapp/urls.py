ab84204bfa1a11d1f1ef256706ab23529d07b978
castle <castlesmadeofcode@gmail.com>
2020-06-19 09:37:44 -0500

    ready to merge

--- capstoneapp/urls.py
+++ capstoneapp/urls.py
@@ -8,9 +8,12 @@ urlpatterns = [
     path('business/form', business_form, name='business_form'),
     path('businesses/<int:business_id>/', business_details, name='business'),
     path('businesses/<int:business_id>/form/', business_edit_form, name='business_edit_form'),
-    path('reviews/', review_list, name='reviews'),
+    
+    path('reviews/', create_review, name='reviews'),
     path('review/form', review_form, name='review_form'),
+    path('reviews/<int:review_id>/', review_details, name='review'),
     path('reviews/<int:review_id>/form/', review_edit_form, name='review_edit_form'),
+
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/register', register, name='register'),
     path('logout/', logout_user, name='logout'),
