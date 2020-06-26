07bb1cd607cdccb667634532b3dcaadffb954c28
castle <castlesmadeofcode@gmail.com>
2020-06-26 10:10:54 -0500

    added carrousel to home page

--- capstoneapp/urls.py
+++ capstoneapp/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/register', register, name='register'),
     path('logout/', logout_user, name='logout'),
-    path('', business_list, name='home'),
+    path('', home, name='home'),
 ]
--- capstoneapp/views/home.py
+++ capstoneapp/views/home.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
+from capstoneapp.models import Business, Review
+from .businesses.business_details import get_Avg
 
 
 def home(request):
     if request.method == 'GET':
+        all_businesses = Business.objects.all()
+
+    if request.method == 'GET':
+        all_businesses = Business.objects.all()
+        all_reviews = Review.objects.all()
+
+        for business in all_businesses:
+            reviews = Review.objects.filter(business_id=business.id)
+            get_avg = get_Avg(reviews)
+            business.review_avg = get_avg
+        sorted_businesses = sorted(
+            all_businesses, key=lambda business: (business.review_avg), reverse=True)
+        print(sorted_businesses)
+
         template = 'home.html'
-        context = {}
+        context = {
+            'all_businesses': sorted_businesses,
+        }
 
         return render(request, template, context)
